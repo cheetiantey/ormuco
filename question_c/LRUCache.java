77fcf39bbbfc682dcf36b867deef49000329461c
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-09-14 15:21:23 -0600

    Fix LRUCache.java

--- question_c/LRUCache.java
+++ question_c/LRUCache.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class LRUCache {
-    Map<Integer, ListNode> hashtable = new HashMap<Integer, ListNode>();
+    HashMap<Integer, ListNode> hashtable = new HashMap<Integer, ListNode>();
     ListNode head;
     ListNode tail;
 
     int totalItemsInCache;
     int maxCapacity;
 
-    public LRUCache(int maxCapacity) {
+    public LRUCache (int maxCapacity) {
     // Cache starts empty and capacity is set by client
     totalItemsInCache = 0;
     this.maxCapacity = maxCapacity;
@@ -22,107 +22,99 @@ public class LRUCache {
     tail.prev = head;
     }
 
-    public Integer get(int key) {
+    public Integer get (int key) {
     ListNode node = hashtable.get(key);
 
     if (node == null) {
         return null;
     }
 
-    // Item has been accessed. Move to the front of the cache
-    moveToHead(node);
+    // Item has been accessed, move to the front of the cache
+    moveToFront(node);
 
     return node.value;
     }
 
-    public void put(int key, int value) {
+    public void put (int key, int value) {
     ListNode node = hashtable.get(key);
-
+    
+    // Add the item if the item isn't already in the cache
     if (node == null) {
-        // Item not found, create a new entry
         ListNode newNode = new ListNode();
         newNode.key = key;
         newNode.value = value;
 
-        // Add to the hashtable and the actual list that represents the cache
+        // Add to the key to a hashtable and the value to a list
         hashtable.put(key, newNode);
         addToFront(newNode);
         totalItemsInCache++;
 
-        // If over capacity remove the LRU item
+        // If we've exceeded the capacity of the cache, remove the 
+        // Least recently used item
         if (totalItemsInCache > maxCapacity) {
         removeLRUEntry();
         }
     } else {
-        // If item is found in the cache, just update it and move it to the head of the list
+        // If the item is in the cache, move it to the front of the cache
         node.value = value;
-        moveToHead(node);
+        moveToFront(node);
     }
     }
 
+    // Remove an entry from the cache
     private void removeLRUEntry() {
-    ListNode tail = popTail();
+        ListNode tailItem = tail.prev;
+        removeFromList(tail.prev);
 
-    hashtable.remove(tail.key);
-    --totalItemsInCache;
+        hashtable.remove(tailItem.key);
+        totalItemsInCache--;
     }
 
-    private ListNode popTail() {
-    ListNode tailItem = tail.prev;
-    removeFromList(tailItem);
 
-    return tailItem;
-    }
-
+    // Add a new item to the front of the cache
     private void addToFront(ListNode node) {
-    // Wire up the new node being to be inserted
-    node.prev = head;
-    node.next = head.next;
+        node.prev = head;
+        node.next = head.next;
 
-    /*
-        Re-wire the node after the head. Our node is still sitting "in the middle of nowhere".
-        We got the new node pointing to the right things, but we need to fix up the original
-        head & head's next.
-
-        head <-> head.next <-> head.next.next <-> head.next.next.next <-> ...
-        ^            ^
-        |- new node -|
-
-        That's where we are before these next 2 lines.
-    */
-    head.next.prev = node;
-    head.next = node;
+        head.next.prev = node;
+        head.next = node;
     }
 
-    private void removeFromList(ListNode node) {
-    ListNode savedPrev = node.prev;
-    ListNode savedNext = node.next;
+    // Remove an item from the cache
+    private void removeFromList (ListNode node) {
+        ListNode savedPrev = node.prev;
+        ListNode savedNext = node.next;
 
-    savedPrev.next = savedNext;
-    savedNext.prev = savedPrev;
+        savedPrev.next = savedNext;
+        savedNext.prev = savedPrev;
     }
 
-    private void moveToHead(ListNode node) {
-    removeFromList(node);
-    addToFront(node);
+    // Move the item that we've just "touched" to the front
+    private void moveToFront (ListNode node) {
+        removeFromList(node);
+        addToFront(node);
     }
 
     private class ListNode {
-    int key;
-    int value;
+        int key;
+        int value;
 
-    ListNode prev;
-    ListNode next;
+        ListNode prev;
+        ListNode next;
     }
 
     public static void main (String args[]) {
-        System.out.println("Test");
+        
+        // Set up a LRU Cahce of size 3
+        // Add 4 items into the cache
         LRUCache lru = new LRUCache(3);
+        System.out.println("Adding 4 items to the cache of size 3...");
         lru.put(1, 100);
         lru.put(2, 200);
         lru.put(3, 300);
         lru.put(4, 400);
-        System.out.println(lru.get(2));
+        System.out.println("Trying to get the first item being put in...(Should be null): " + lru.get(1));
+        System.out.println("Trying to get the value of key '2'...(Should be 200): " + lru.get(2));
     }
 }
 
